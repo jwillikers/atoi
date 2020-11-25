3d150249db192f7ac5611a8272c96b48fd0e5a87
Jordan Williams <19399197+jwillikers@users.noreply.github.com>
2020-11-25 16:03:48 -0600

    Place export macro after the extern declaration

--- src/atoi.h
+++ src/atoi.h
@@ -4,7 +4,7 @@
 // Convert a string to an integer.
 // Zero is returned if a null pointer is passed in or a conversion can not be performed.
 #ifdef __cplusplus
-ATOI_EXPORT extern "C" int my_atoi(char const *c);
+extern "C" ATOI_EXPORT int my_atoi(char const *c);
 #else
 ATOI_EXPORT int my_atoi(char const *c);
 #endif
