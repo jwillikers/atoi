5cd5e0233d93f9a3d347ff45576ccdd35cba0f46
Jordan Williams <19399197+jwillikers@users.noreply.github.com>
2020-11-25 10:25:44 -0600

    Explicitly export shared library symbols
    This is just standard practice.

--- src/atoi.h
+++ src/atoi.h
@@ -1,9 +1,10 @@
 #pragma once
+#include <atoi/export.h>
 
 // Convert a string to an integer.
 // Zero is returned if a null pointer is passed in or a conversion can not be performed.
 #ifdef __cplusplus
-extern "C" int my_atoi(char const *c);
-#e
-int my_atoi(char const *c);
+ATOI_EXPORT extern "C" int my_atoi(char const *c);
+#else
+ATOI_EXPORT int my_atoi(char const *c);
 #endif
